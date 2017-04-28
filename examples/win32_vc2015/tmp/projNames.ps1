5db419ac3bdda9404404ca56b203c648c954095f
soundbytes <soundbytes@outlook.com>
2017-04-28 13:53:07 +0200

    Add project files.

---
+++ examples/win32_vc2015/tmp/projNames.ps1
@@ -0,0 +1,27 @@
+﻿$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$PSScriptRoot += "\"
+
+cd $PSScriptRoot
+
+
+function tmpl2new($newName){
+    $projFile = "$newName.vcxproj"
+    $filtFile = "$newName.vcxproj.filters"
+    (Get-Content template.vcxproj) | ForEach-Object { $_ -replace "template", $newName } | Set-Content $projFile
+    (Get-Content template.vcxproj.filters) | ForEach-Object { $_ -replace "template", $newName } | Set-Content $filtFile
+}
+
+$reader = [System.IO.File]::OpenText( $PSScriptRoot + "projNames.txt")
+try {
+    for() {
+        $line = $reader.ReadLine()
+        if ($line -eq $null) { break }
+        # process the line
+        tmpl2new($line)
+    }
+}
+finally {
+    $reader.Close()
+}
+
+
---
+++ examples/win32_vc2015/tmpl2new.ps1
@@ -0,0 +1,11 @@
+﻿param(
+[Parameter(Mandatory=$true)][String] $newName
+)
+$projFile = "$newName.vcxproj"
+$filtFile = "$newName.vcxproj.filters"
+
+#copy template.vcxproj $projFile
+#copy template.vcxproj.filters $filtFile
+
+(Get-Content template.vcxproj) | ForEach-Object { $_ -replace "template", "$newName" } | Set-Content $projFile
+(Get-Content template.vcxproj.filters) | ForEach-Object { $_ -replace "template", "$newName" } | Set-Content $filtFile
